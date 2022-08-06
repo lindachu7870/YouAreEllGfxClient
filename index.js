5024ec8de0019419258484287f33ff995b0744bc
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-06 15:44:54 -0400

    done

--- index.js
+++ index.js
@@ -4,9 +4,61 @@ let userId = "lindachu7870";
 
 const messageService = new MessageService(userId);
 
-window.addEventListener("load", function () {
-    document.getElementById("greeting").innerHTML = `Welcome ${userId}!`;
-    messageService.getAllMessages();
 
+window.addEventListener("load", function () {
+
+    document.getElementById("greeting").innerHTML = `Welcome ${userId}!`;
+    messageService.getAllMessages()
+        .then(successCallback, errorCallback);
+
+    function successCallback(response) {
+        populateMessages(response);
+    }
+
+    function errorCallback(response) {
+        console.log(response);
+    }
+}
+
+function populateMessages(messages) {
+    messages.forEach(message => {
+        addMessageToThread(message);
+    })
+}
+
+function createFormListener() {
+    const form = document.getElementById("new-message-form");
+
+    form.onsubmit = function (event) {
+        event.preventDefault();
+
+        const data = {
+            fromid: userId,
+            message: form.message.value
+        };
+
+        messageService.createNewMessage(data)
+            .then(successCallback, errorCallback);
+
+        function successCallback(response) {
+            addMessageToThread(response);
+        }
+
+        function errorCallback(response) {
+            console.log(response);
 });
+
+function addMessageToThread(message) {
+    const messageListItem = document.createElement("LI");
+    const userIdHeading = document.createElement("h3");
+    const messageParagraph = document.createElement("p");
+    const messageContent = document.createTextNode(message.message);
+    const userIdContent = document.createTextNode(message.fromid);
+    userIdHeading.appendChild(userIdContent);
+    messageParagraph.appendChild(messageContent);
+    messageListItem
+        .appendChild(userIdHeading)
+        .appendChild(messageParagraph);
+    document.getElementById("message-list").appendChild(messageListItem);
+}
 
--- message-service.js
+++ message-service.js
@@ -1,18 +1,58 @@
 export default class MessageService {
 
 getAllMessages() {
-    let request = new XMLHttpRequest();
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 300) {
-            console.log(JSON.parse(request.responseText));
-        } else {
-            console.log('Error: ' + request.status);
-        }
-    };
+    const request = new XMLHttpRequest();
 
-    request.open("GET", "http://zipcode.rocks:8085/messages");
+    return new Promise(function (resolve, reject) {
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 300) {
+                const threads = JSON.parse(request.responseText);
+                resolve(threads);
+            } else {
+                reject({
+                    status: request.status,
+                    statusText: request.statusText
+                });
+            }
+        };
 
-    request.send();
+        request.open("GET", "http://zipcode.rocks:8085/messages");
+
+        request.send();
+    }).then(successCallback, errorCallback);
+
+    function successCallback(response) {
+        console.log(response);
+    }
+
+    function errorCallback(response) {
+        console.log(response);
+    }
 }
+
+createNewMessage(message) {
+    const request = new XMLHttpRequest();
+
+    return new Promise(function (resolve, reject) {
+        // Setup our listener to process compeleted requests
+        request.onload = function () {
+            // Process the response
+            if (request.status >= 200 && request.status < 300) {
+                // If successful
+                resolve(JSON.parse(request.responseText));
+            } else {
+                reject({
+                    status: request.status,
+                    statusText: request.statusText
+                });
+            }
+        };
+
+        request.open("POST", `http://zipcode.rocks:8085/ids/${message.fromid}/messages`);
+
+        request.send(JSON.stringify(message));
+    });
+}
+
 
 }
